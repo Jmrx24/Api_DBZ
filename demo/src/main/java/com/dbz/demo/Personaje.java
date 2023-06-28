a57e2c2d465d639b08e105fc809760cb4348ac54
Jmrx24 <Jmrx24@gmail.com>
2023-06-28 17:46:02 +0200

    Clase personaje mejorada

--- demo/src/main/java/com/dbz/demo/Personaje.java
+++ demo/src/main/java/com/dbz/demo/Personaje.java
@@ -6,14 +6,17 @@ public class Personaje {
     private String origin;
     private String species;
 
+    private String url_scrap;
+
     public Personaje() {
     }
 
-    public Personaje(String name, String birthday, String origin, String species) {
+    public Personaje(String name, String birthday, String origin, String species, String url_scrap) {
         this.name = name;
         this.birthday = birthday;
         this.origin = origin;
         this.species = species;
+        this.url_scrap = url_scrap;
     }
 
     public String getName() {
@@ -26,6 +29,10 @@ public class Personaje {
 
     public String getBirthday() {
         return birthday;
+    }
+
+    public String getUrl_scrap() {
+        return url_scrap;
     }
 
     public void setBirthday(String birthday) {
@@ -46,5 +53,9 @@ public class Personaje {
 
     public void setSpecies(String species) {
         this.species = species;
+    }
+
+    public void setUrl_scrap(String url_scrap) {
+        this.url_scrap = url_scrap;
     }
 }
